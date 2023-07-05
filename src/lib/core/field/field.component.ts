20f6ec10c1f623513f4ca566cf07a3cb5a2e1a4e
Inon Kaplan <inonkp@gmail.com>
2023-07-05 09:37:58 +0300

    field component is on push

--- src/lib/core/field/field.component.ts
+++ src/lib/core/field/field.component.ts
@@ -1,9 +1,10 @@
-import { Component, ComponentRef, forwardRef, Inject, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ComponentRef, forwardRef, Inject, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'deco-field',
   templateUrl: './field.component.html',
   styleUrls: ['./field.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FieldComponent implements OnInit {
 
