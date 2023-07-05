733b91256acbaf5e2b82092ba3046797a6dba784
Inon Kaplan <inonkp@gmail.com>
2023-07-05 09:35:22 +0300

    added root form token

--- src/lib/deco-form.component.ts
+++ src/lib/deco-form.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component, ComponentRef, ContentChild, EventEmitter, forwardRef, Inject, inject, Injector, Input, Output, ViewChild, ViewContainerRef } from '@angular/core';
-import { FormArray, FormGroup, NgForm } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, ComponentRef, ContentChild, EventEmitter, forwardRef, Inject, inject, InjectionToken, Injector, Input, Optional, Output, ViewChild, ViewContainerRef } from '@angular/core';
+import { FormArray, FormGroup, FormGroupDirective, NgForm } from '@angular/forms';
 import { FieldComponent } from './core/field/field.component';
 import { DecoFormDirective } from './deco-form.directive';
 import { DecoFormNode } from './builders/deco-form-node';
+import { FORM_TARGET_TOKEN } from './builders/deco-form-tokens';
 
+export const ROOT_FORM_GROUP_TOKEN = new InjectionToken<FormGroup>('root-form-group');
 
 @Component({
   selector: 'ng-deco-form',
@@ -27,8 +29,10 @@ export class DecoFormComponent {
   fields: DecoFormNode[];
   formGroup: FormGroup;
 
-  constructor() {
-    this.formGroup = new FormGroup({});
+  constructor(@Optional() @Inject(ROOT_FORM_GROUP_TOKEN) private rootFormGroup: FormGroup,
+    @Inject(FORM_TARGET_TOKEN) target: any) {
+    this.formTarget = target;
+    this.formGroup = rootFormGroup ?? new FormGroup({});
   }
 
 
